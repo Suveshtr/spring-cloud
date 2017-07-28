488bc7a29c2b083484639fbf75d5d93866cf0947
Suveshtr <suvesh.trivedi@gmail.com>
2017-07-28 20:21:47 +0530

    Initial commit

---
+++ springcloud-m2-configserver-client/src/main/java/git/configClient/SpringcloudM2ConfigserverClientApplication.java
@@ -0,0 +1,12 @@
+package git.configClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ConfigserverClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ConfigserverClientApplication.class, args);
+	}
+}
