55b67e18e998eb7841182435409806165c4583eb
Suveshtr <suvesh.trivedi@gmail.com>
2017-08-05 12:18:55 +0530

    Rename the config server and config client

--- springcloud-configserver-client/src/main/java/git/configClient/RateController.java
+++ springcloud-configserver-client/src/main/java/git/configClient/RateController.java
@@ -1,11 +1,13 @@
 package git.configClient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 	
 	@Value("${rate}")
@@ -17,6 +19,9 @@ public class RateController {
 	@Value("${tollstart}")
 	String tollstart;
 	
+	@Value("${connstring}")
+	String connstring;
+	
 	@RequestMapping("/rate")
 	public String getRate(Model m) {
 		//pass back following values to the ui
@@ -24,7 +29,7 @@ public class RateController {
 		m.addAttribute("rateamount", rate);
 		m.addAttribute("lanes", lanecount);
 		m.addAttribute("tollstart", tollstart);
-		
+		m.addAttribute("connstring", connstring);
 		//return name of the view
 		
 		return "rateview";	
--- springcloud-configserver-client/src/main/java/git/configClient/SpringcloudM2ConfigserverClientApplication.java
+++ springcloud-configserver-client/src/main/java/git/configClient/SpringcloudM2ConfigserverClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcloudM2ConfigserverClientApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringcloudM2ConfigserverClientApplication.class, args);
 	}
 }
